080908f713074f15caad7573d7a6cc2fb0611d23
xgiral-metacodi <65396010+xgiral-metacodi@users.noreply.github.com>
2023-09-25 18:37:57 +0200

    auto-commit

--- ios/Plugin/CapacitorElectronMetacodiPlugin.m
+++ ios/Plugin/CapacitorElectronMetacodiPlugin.m
@@ -7,4 +7,10 @@ CAP_PLUGIN(CapacitorElectronMetacodiPlugin, "CapacitorElectronMetacodi",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(requestPermissionsCalendar, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(checkPermissionCalendar, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(createCalendar, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(createEvent, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(updateEvent, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(deleteEvent, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(listCalendars, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(listEvents, CAPPluginReturnPromise);
 )

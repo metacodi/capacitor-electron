e1fbd2385cbf532c28dbd4ba4428ba2676f66aae
xgiral-metacodi <65396010+xgiral-metacodi@users.noreply.github.com>
2023-10-03 09:53:31 +0200

    auto-commit

--- android/src/main/java/com/metacodi/plugin/capacitor/electron/CapacitorElectronMetacodiPlugin.java
+++ android/src/main/java/com/metacodi/plugin/capacitor/electron/CapacitorElectronMetacodiPlugin.java
@@ -1,12 +1,50 @@
 package com.metacodi.plugin.capacitor.electron;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.provider.CalendarContract.Events;
+import android.provider.CalendarContract.Instances;
+import android.provider.CalendarContract.Calendars;
+import android.text.TextUtils;
+import android.text.format.DateUtils;
+import android.util.Log;
+
 import com.getcapacitor.JSObject;
+import com.getcapacitor.NativePlugin;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.Permission;
+import com.getcapacitor.annotation.PermissionCallback;
+import com.getcapacitor.PermissionState;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.CapacitorPlugin;
 
-@CapacitorPlugin(name = "CapacitorElectronMetacodi")
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
+import com.google.gson.Gson;
+
+@CapacitorPlugin(
+    name = "CapacitorElectronMetacodi",
+    permissions = {
+        @Permission(strings = { Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR }, alias = "calendar")
+    }
+)
 public class CapacitorElectronMetacodiPlugin extends Plugin {
 
     private CapacitorElectronMetacodi implementation = new CapacitorElectronMetacodi();
@@ -17,6 +55,37 @@ public class CapacitorElectronMetacodiPlugin extends Plugin {
 
         JSObject ret = new JSObject();
         ret.put("value", implementation.echo(value));
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void checkCalendarPermission(PluginCall call) {
+        String status = getPermissionState("calendar");
+        
+        JSObject ret = new JSObject();
+        ret.put("status", status);
+        call.resolve(ret);
+    }
+
+    protected void requestPermissionsCalendar(PluginCall call) {
+        requestPermissionForAlias("calendar", call, "calendarPermsCallback");
+    }
+
+    @PermissionCallback
+    private void calendarPermsCallback(PluginCall call) {
+        if (!(getPermissionState("calendar") == PermissionState.GRANTED)) {
+            call.reject("Permission is required");
+        }
+    }
+
+    @PluginMethod
+    public void requestCalendarPermissions(PluginCall call) {
+        requestPermissionsCalendar(call);
+        
+        String status = getPermissionState("calendar");
+        
+        JSObject ret = new JSObject();
+        ret.put("status", status);
         call.resolve(ret);
     }
 }

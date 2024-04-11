0563bd597871ff8274e57a7e38735e19282dbada
xgiral-metacodi <65396010+xgiral-metacodi@users.noreply.github.com>
2024-04-11 10:41:26 +0200

    auto-commit

--- src/web.ts
+++ src/web.ts
@@ -38,7 +38,7 @@ export class CapacitorElectronMetacodiWeb extends WebPlugin implements Capacitor
 
   async stopSound(): Promise<void> { return; };
 
-  async execute(options: { command: string }): Promise<void> { 
+  async execute(options: { command: string }): Promise<any> { 
     console.log('not implemented on web', options);
     return;
   }
